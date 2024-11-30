54029b0944b8de88ca2284f92b44ba9221356ec2
Márcio da Mota Xerém <mxerem@icloud.com>
2024-11-30 10:54:20 -0300

    segundo commit do hm-3-4

--- hm-3-4-modificado/src/main.ts
+++ hm-3-4-modificado/src/main.ts
@@ -1,3 +1,4 @@
+import './style.css';
 import Data from './Data';
 import Voo from './Voo';
 
@@ -11,26 +12,29 @@ function compararDatas() {
 
   const data1 = new Data(data1Input);
   const data2 = new Data(data2Input);
-  
+  const painel1 = _$('resultadoComparacao');
+    
   const resultadoComparacao = data1.compara(data2);
   const resultadoTexto = resultadoComparacao === 0 ? 'Datas iguais' : (resultadoComparacao > 0 ? 'Data 1 é posterior à Data 2' : 'Data 1 é anterior à Data 2');
 
-  (_$('resultadoComparacao') as HTMLElement).innerText = resultadoTexto;  
+  (painel1 as HTMLElement).innerText = resultadoTexto;  
 }
 
 function informarVoo() {
   const numeroVooInput = parseInt((_$('numeroVoo') as HTMLInputElement).value, 10);
   const dataVoo = new Data('25/12/2024'); // Data fixa para exemplo
+  const painel2 = _$('informacoesVoo');
   
   voo = new Voo(numeroVooInput, dataVoo);
   const informacoesVoo = `Número do voo: ${voo.getVoo()} - Data do voo: ${voo.getData().getDia()}/${voo.getData().getMes()}/${voo.getData().getAno()}`;
 
-  (_$('informacoesVoo') as HTMLElement).innerText = informacoesVoo;
+  (painel2 as HTMLElement).innerText = informacoesVoo;
 }
 
 function testarData(dataInputId: string, resultadoId: string) {
   const dataInput = (_$(dataInputId) as HTMLInputElement).value;
   const data = new Data(dataInput);
+  const painel3 = _$(resultadoId)
 
   const dia = data.getDia();
   const mes = data.getMes();
@@ -41,41 +45,52 @@ function testarData(dataInputId: string, resultadoId: string) {
     ? 'Data inválida: 01/01/0001'
     : `Data válida: ${dataFormatada} - Ano bissexto: ${data.isBissexto() ? 'Sim' : 'Não'}`;
 
-  (_$(resultadoId) as HTMLElement).innerText = resultadoTexto;
+  (painel3 as HTMLElement).innerText = resultadoTexto;
 }
 
 function ocuparCadeira() {
   const assentoInput = parseInt((_$('assento') as HTMLInputElement).value, 10);
   voo.ocupa(assentoInput);
   const resultadoTexto = `Cadeira ${assentoInput} ocupada.`;
-  (_$('resultadoCadeira') as HTMLElement).innerText = resultadoTexto;
+  const painel4 = _$('resultadoCadeira');
+
+  (painel4 as HTMLElement).innerText = resultadoTexto;
 }
 
 function verificarCadeira() {
   const assentoInput = parseInt((_$('assento') as HTMLInputElement).value, 10);
   const ocupado = voo.verifica(assentoInput);
   const resultadoTexto = ocupado ? `Cadeira ${assentoInput} está ocupada.` : `Cadeira ${assentoInput} está livre.`;
-  (_$('resultadoCadeira') as HTMLElement).innerText = resultadoTexto;
+  const painel5 = _$('resultadoCadeira');
+
+  (painel5 as HTMLElement).innerText = resultadoTexto;
 }
 
 function proximaCadeiraLivre() {
   const proximaLivre = voo.proximoLivre();
   const resultadoTexto = `Próxima cadeira livre: ${proximaLivre}.`;
-  (_$('resultadoProximaLivre') as HTMLElement).innerText = resultadoTexto;
+  const painel6 = _$('resultadoProximaLivre');
+
+  (painel6 as HTMLElement).innerText = resultadoTexto;
 }
 
 function quantidadeCadeirasVagas() {
   const vagas = voo.vagas();
   const resultadoTexto = `Quantidade de cadeiras vagas: ${vagas}.`;
-  (_$('resultadoQuantidadeVagas') as HTMLElement).innerText = resultadoTexto;
+  const painel7 = _$('resultadoQuantidadeVagas');
+
+  (painel7 as HTMLElement).innerText = resultadoTexto;
 }
 
 // Adicionar eventos aos botões
-(_$('comparar') as HTMLElement).addEventListener('click', compararDatas);
-(_$('informarVoo') as HTMLElement).addEventListener('click', informarVoo);
-(_$('testarData1') as HTMLElement).addEventListener('click', () => testarData('data1', 'resultadoData1'));
-(_$('testarData2') as HTMLElement).addEventListener('click', () => testarData('data2', 'resultadoData2'));
-(_$('ocuparCadeira') as HTMLElement).addEventListener('click', ocuparCadeira);
-(_$('verificarCadeira') as HTMLElement).addEventListener('click', verificarCadeira);
-(_$('proximaLivre') as HTMLElement).addEventListener('click', proximaCadeiraLivre);
-(_$('quantidadeVagas') as HTMLElement).addEventListener('click', quantidadeCadeirasVagas);
+const btnComparar = _$('comparar'); const btninformarVoo = _$('informarVoo'); const btntestarData1 = _$('testarData1');
+const btntestarData2 = _$('testarData2'); const btnocuparCadeira = _$('ocuparCadeira'); const btnverificarCadeira = _$('verificarCadeira');
+const btnproximaLivre = _$('proximaLivre'); const btnquantidadeVagas = _$('quantidadeVagas');
+(btnComparar as HTMLElement).addEventListener('click', compararDatas);
+(btninformarVoo as HTMLElement).addEventListener('click', informarVoo);
+(btntestarData1 as HTMLElement).addEventListener('click', () => testarData('data1', 'resultadoData1'));
+(btntestarData2 as HTMLElement).addEventListener('click', () => testarData('data2', 'resultadoData2'));
+(btnocuparCadeira as HTMLElement).addEventListener('click', ocuparCadeira);
+(btnverificarCadeira as HTMLElement).addEventListener('click', verificarCadeira);
+(btnproximaLivre as HTMLElement).addEventListener('click', proximaCadeiraLivre);
+(btnquantidadeVagas as HTMLElement).addEventListener('click', quantidadeCadeirasVagas);
